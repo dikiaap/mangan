fe13926eb87b5ee7e88f428419730c712e92fe1e
Diki Andriansyah A P <diki1aap@gmail.com>
2015-10-17 08:26:06 +0700

    EXPLOOOOOOOOOOOOOOSSSSIIIIIIIIIIIIOOOOOONNNNNNNNNNN

---
+++ js/min.js
@@ -0,0 +1,5 @@
+console.log(
+	"%cL M A O\n%chttps://github.com/dikiaap",
+	"color: #666; font-weight: bold; font-size: 21px; line-height: 2;",
+	"color: #4078c0; font-size: 14px; line-height: 2;"
+);
